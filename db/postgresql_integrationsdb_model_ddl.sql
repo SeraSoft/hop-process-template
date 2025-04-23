bf90861fb3120bf2f840705de68293865f3354a3
sramazzina <sergio.ramazzina@serasoft.it>
2025-04-23 19:22:04 +0200

    Latest improvement and bugfixes. WIP

--- db/postgresql_integrationsdb_model_ddl.sql
+++ db/postgresql_integrationsdb_model_ddl.sql
@@ -34,7 +34,9 @@ CREATE  TABLE config.integrations_processes (
 	short_name           varchar(32)  NOT NULL  ,
 	description          varchar(1024)    ,
 	project              varchar(64)  NOT NULL  ,
-	is_active            char(1) DEFAULT 'Y'::bpchar NOT NULL  ,
+	is_active            char(1) DEFAULT 'Y'::bpchar NOT NULL,
+	process_lock       char(1) DEFAULT 'S'::bpchar NOT NULL,
+	last_instance_started_at timestamp,
 	CONSTRAINT integrations_processes_pk PRIMARY KEY ( id )
  );
 
